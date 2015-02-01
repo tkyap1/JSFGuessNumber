e359c747f3f1597351e81f32d28f2d44055ca89d
tungyap <tkyap1@yahoo.com>
2015-01-31 22:55:43 -0500

    Updated response text to include the word bro.

--- JSFGuessNumber/src/guessNumber/UserNumberBean.java
+++ JSFGuessNumber/src/guessNumber/UserNumberBean.java
@@ -31,7 +31,7 @@ public class UserNumberBean implements Serializable {
 
 	public String getResponse() {
 		if ((userNumber != null) && (userNumber.compareTo(randomInt) == 0)) {
-			return "Yay! You got it!";
+			return "Yay! You got it bro!";
 		} else {
 			return "Sorry, " + userNumber + " is incorrect.";
 		}
